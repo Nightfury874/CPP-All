e3fadf9d0c5308bddc4437d672e1f6365a520c45
unknown <jibaba874@gmail.com>
2021-12-19 21:10:02 +0530

    strings

---
+++ user_input.cpp
@@ -0,0 +1,22 @@
+// # include <iostream>
+// using namespace std ;
+// int main ()
+// {
+//     int age;
+//     cout << " enter your age"  ;
+//     cin >> age;
+//     cout << " Your age is" << age ;
+
+// }
+
+// Insert string
+# include <iostream>
+using namespace std ;
+int main ()
+{
+    int age;
+    string name;
+    cout << " enter your name"  ;
+    getline(cin, name) ;
+    cout << " Your name is" << name ;
+}
