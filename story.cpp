a11e3c5676f08e88c276c5048d61eab1ae0757b5
unknown <jibaba874@gmail.com>
2021-12-19 11:15:10 +0530

    aa

---
+++ story.cpp
@@ -0,0 +1,25 @@
+// # include <iostream>
+// using namespace std ;
+// int main ()
+
+// {
+//     cout <<" the firts lines"<<  endl ;
+//     return 0;
+// }
+
+// Creating the variables
+
+# include <iostream>
+using namespace std ;
+
+int main ()
+{
+
+    string CharName = "Nihar";
+    int age = 20 ;
+
+    cout << " the name is " << CharName << endl ;
+    cout << " his age is " << age << endl ;
+
+    return 0;
+}
---
+++ strings.cpp
@@ -0,0 +1,14 @@
+ # include <iostream>
+ using namespace std;
+
+ int main ()
+ {
+     string Name = "geekforgeeks" ;
+     // will give output of the location of the first index
+     cout << Name.find("geek") << endl ;
+     cout << Name.substr(2,4) << endl;
+     
+
+
+
+ }
