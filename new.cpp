36e61771928131044dfb76392112f0710a65fd04
unknown <jibaba874@gmail.com>
2021-12-20 07:46:26 +0530

    new

---
+++ new.cpp
@@ -0,0 +1,64 @@
+// # include <iostream>
+
+// using namespace std;
+
+// int main ()
+// {
+//     int arr [] =  {4,8,25,4,2,4} ;
+
+//     cout << arr[5] ;
+//     return 0;
+// }
+
+// buildinga a better calculator
+
+// # include <iostream>
+// using namespace std ;
+
+// int main ()
+// {
+//     char op;
+//     int num1, num2, result;
+//     cout << " Enter the first number" ;
+//     cin >> num1 ;
+//     cout << "Enter the operator" ;
+//     cin >> op ;
+//     cout << " Enter the second number" ;
+//     cin >> num2 ;
+
+//     if ( op == '+') {
+//         result = num1 + num2 ;
+//     }
+//     if ( op == '-') {
+//         result = num1 - num2 ;
+//     }
+//     if ( op == '/') {
+//         result = num1 / num2 ;
+//     }
+//     if ( op == '*') {
+//         result = num1 * num2 ;
+//     }
+//     else {
+//         cout << " Invalid Input" ;
+//     }
+
+//     cout << result ;
+//     return 0;
+// }
+
+// Loops
+
+// # include <iostream>
+// using namespace std ;
+// int main (){
+//     int index = 0;
+//     while (index < 5)
+//     {
+//        cout << index << endl ;
+//        index ++ ;
+//     }
+    
+// }
+
+
+//  Arrays writing and reading
