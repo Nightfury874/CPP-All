9f19cf2240e424d47e2bf11ec44d45cee00028cd
unknown <jibaba874@gmail.com>
2021-12-20 10:44:31 +0530

    aa

--- pointers.cpp
+++ pointers.cpp
@@ -1,34 +1,101 @@
+// # include <iostream>
+// using namespace std;
+// void printNum(int *NumPtr)
+// {
+//     cout << *NumPtr<< endl;
+// }
+// void printLetter(char *letter)
+// {
+//     cout << *letter<< endl;
+// }
+
+// void print (void*ptr, char type){
+//     switch (type)
+//     {
+//     case 'i' : cout << *((int*) ptr) << endl;
+//         break;
+    
+//     case 'c' : cout<< *((char*) ptr) << endl;
+//         break;
+    
+    
+//     }
+// }
+// int main()
+// {
+//     int n =5;
+//     char letter = 'b' ;
+//     // printNum(&n);
+
+//     // printLetter(&letter) ;
+//     print (&n,' i') ;
+//     print (&letter,' c') ;
+//     return 0;
+// }
+
+
+
+// Arrays with pointer
+
+
 # include <iostream>
+
 using namespace std;
-void printNum(int *NumPtr)
+
+int getMin(int arr[] , int size)
 {
-    cout << *NumPtr<< endl;
+      int min = arr [0];
+      for ( int i =0 ; i<size; i++)
+      {
+          if (arr[i]< min){
+              min = arr [i] ;
+          }
+      }
+
+      return min;
+
 }
-void printLetter(char *letter)
+int getMax (int arr[], int size)
 {
-    cout << *letter<< endl;
+    int max = 0;
+    for (int i = 0; i<size ; i++)
+    {
+        if( arr[i] > max)
+        {
+            max = arr [i];
+        }
+    }
+    return max;
 }
 
-void print (void*ptr, char type){
-    switch (type)
+void getMinMax (int arr[], int size, int* min, int* max )
+{
+    for (int i = 0; i<size ; i++)
     {
-    case 'i' : cout << *((int*) ptr) << endl;
-        break;
-    
-    case 'c' : cout<< *((char*) ptr) << endl;
-        break;
-    
-    
+        if( arr[i] > *max)
+        {
+            *max = arr [i];
+        }
+    }
+    for (int i = 0; i<size ; i++)
+    {
+        if( arr[i] < *min)
+        {
+            *min = arr [i];
+        }
     }
 }
-int main()
+int main ()
 {
-    int n =5;
-    char letter = 'b' ;
-    // printNum(&n);
+    int arr[5]= {2,8,5,7,6} ;
+    cout << " min element is " << getMin(arr, 5) << endl;
+    cout << " max element is " << getMax(arr, 5) << endl ;
 
-    // printLetter(&letter) ;
-    print (&n,' i') ;
-    print (&letter,' c') ;
+    int min = arr[0];
+    int max = arr[0];
+    getMinMax (arr, 5, &min, &max);
+
+    cout << "The max number is " << max << " and min number is " << min << endl;
+
     return 0;
 }
