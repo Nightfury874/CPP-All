9e55b58fc7f24791d0bc222342fb1c0bd11eb911
unknown <jibaba874@gmail.com>
2021-12-22 17:45:32 +0530

    aa

--- vector.cpp
+++ vector.cpp
@@ -3,29 +3,57 @@
 
 using namespace std;
 
+// int main()
+// {
+//     vector <int>n;
+//     n.assign(9,1);
+
+//     cout<< "The contents are : ";
+
+//     for (int i = 0; i< n.size(); i++)
+//     {
+//         cout<<n[i] << " "<< endl;
+//     }
+//     n.push_back(2);
+//     int num = n.size();
+
+//     cout << "Last element is :" << n[num-1] << endl;
+//      for (int i = 0; i< n.size(); i++)
+//     {
+//         cout<<n[i] << " "<< endl;
+//     }
+//     n.pop_back();
+//      for (int i = 0; i< n.size(); i++)
+//     {
+//         cout<<n[i] << " "<< endl;
+//     }
+//     cout <<"new line from here on" ;
+
+//     n.insert(n.begin() + 4, 10);
+//      for (int i = 0; i< n.size(); i++)
+//     {
+//         cout<<n[i] << " "<< endl;
+//     }
+
+//     n.clear();
+//     cout << "size is "<< n.size();
+
+//     return 0;
+// }
+
 int main()
 {
-    vector <int>n;
-    n.assign(9,1);
+    vector<int>n;
+    int a;
 
-    cout<< "The contents are : ";
-
-    for (int i = 0; i< n.size(); i++)
+    for (int i = 0 ; i< 5 ; i++)
     {
-        cout<<n[i] << " "<< endl;
+        cin >> a;
+        n.push_back(a);
     }
-    n.push_back(2);
-    int num = n.size();
-
-    cout << "Last element is :" << n[num-1] << endl;
-     for (int i = 0; i< n.size(); i++)
+    for ( auto a = n.begin(); a != n.end(); a++ )
     {
-        cout<<n[i] << " "<< endl;
-    }
-    n.pop_back();
-     for (int i = 0; i< n.size(); i++)
-    {
-        cout<<n[i] << " "<< endl;
+        cout<<" the elements are " << *a << endl;
     }
     return 0;
 }
