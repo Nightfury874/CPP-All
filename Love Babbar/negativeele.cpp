a3c605a461d3c5fbf906332e0c6d76743014808f
Nihar Jyoti Basisth <jibaba874@gmail.com>
2022-06-07 07:59:19 +0530

    aa

---
+++ Love Babbar/negativeele.cpp
@@ -0,0 +1,12 @@
+//Move all the negative elements to one side of the array
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[] = {-1,-2,-4,-5,-2,0,6,5,4,5,2,3,-4,-5,3,-3,-7,-5};
+    int n =  sizeof(arr)/sizeof(arr[0]);
+    sort(arr,arr+n);
+    for (int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
